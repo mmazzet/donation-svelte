cc50558c8e35b25016d1cd81e55ddc1bf0af4754
Marianna Mazzette <20104120@mail.wit.ie>
2024-04-15 23:39:32 +0100

    introduced card report and donation list

--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-  import { subTitle } from "$lib/stores";
+  import Card from "$lib/ui/Card.svelte";
   import DonateForm from "./DonateForm.svelte";
+  import { subTitle } from "$lib/stores";
 
   subTitle.set("Make a Donation");
 </script>
 
-<DonateForm />
+<Card title="Please Donate">
+  <DonateForm />
+</Card>
