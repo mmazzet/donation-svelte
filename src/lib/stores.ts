fbd7b6272dd5c94db697a9a4e128fd109c115569
Marianna Mazzette <20104120@mail.wit.ie>
2024-04-18 21:54:36 +0100

    added donationService

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,5 +1,5 @@
-// place files you want to import through the `$lib` alias in this folder.
 import { writable } from "svelte/store";
+import type { Session } from "$lib/types/donation-types";
 
+export const currentSession = writable<Session>();
 export const subTitle = writable<string>();
-export const currentSession = writable<string>();
